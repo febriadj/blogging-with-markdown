c4177c1ce6ac0891479c96c0979c99b5410e2fe5
i'mfebri <73212374+febriadj@users.noreply.github.com>
2021-01-17 21:57:19 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('/routes/route'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+})
