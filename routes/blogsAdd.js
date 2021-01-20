920edfe7119deba5a87658c33fc2ac395fd607b0
i'mfebri <73212374+febriadj@users.noreply.github.com>
2021-01-21 03:02:06 +0700

    initial commit

--- routes/blogsAdd.js
+++ routes/blogsAdd.js
@@ -17,32 +17,40 @@ router.route('/blogs/add')
       if (err) console.log(err);
       const { author, title, subject } = fields;
 
-      const configPath = [...title];
-      for ( let i = 0; i < configPath.length; i++ ) {
-        if ( configPath[i] == " " ) {
-          configPath[i] = "-";
+      if ( author.length === 0  ) {
+        res.send('Nama Author Belum Diisi');
+      } else if ( title.length === 0 ) {
+        res.send('Title Belum Diisi');
+      } else if ( subject.length === 0 ) {
+        res.send('Subject Belum Diisi');
+      } else {
+        const configPath = [...title];
+        for ( let i = 0; i < configPath.length; i++ ) {
+          if ( configPath[i] == " " ) {
+            configPath[i] = "-";
+          }
         }
-      }
-      const path = configPath.join("").toLowerCase();
-      
-      const namaFile = new Date();
-      const newfile = `${namaFile.getMilliseconds()}MD${files.file.size}BL${Date.now()}`;
-      mv(files.file.path, `blogs/${newfile}.md`, {mkdirp: true, multiples: true}, err => {
-        if (err) console.log(err);
+        const path = configPath.join("").toLowerCase();
         
-        const date = new Date();
-
-        const hari = ["Minggu","Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
-        const bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-        const time = `${hari[date.getDay()]}, ${date.getDate()} ${bulan[date.getMonth()]} ${date.getFullYear()}`;
-
-        let sql = `INSERT INTO blogs VALUE (0, '${author}', '${title}', '${subject}', '${time}', '${newfile}', '${path}')`
-        db.query(sql, (err, result) => {
+        const namaFile = new Date();
+        const newfile = `${namaFile.getMilliseconds()}MD${files.file.size}BL${Date.now()}`;
+        mv(files.file.path, `blogs/${newfile}.md`, {mkdirp: true, multiples: true}, err => {
           if (err) console.log(err);
-
-          res.redirect('/blogs');
+          
+          const date = new Date();
+  
+          const hari = ["Minggu","Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+          const bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+          const time = `${hari[date.getDay()]}, ${date.getDate()} ${bulan[date.getMonth()]} ${date.getFullYear()}`;
+  
+          let sql = `INSERT INTO blogs VALUE (0, '${author}', '${title}', '${subject}', '${time}', '${newfile}', '${path}')`
+          db.query(sql, (err, result) => {
+            if (err) console.log(err);
+  
+            res.redirect('/blogs');
+          });
         });
-      });
+      };
     });
   })
 
